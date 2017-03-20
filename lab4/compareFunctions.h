5da6553ee2d4d398db1e191eb750bee49ddfc1be
Nicholas Nolan <nknolan@users.noreply.github.com>
2017-03-19 21:12:45 -0400

    Add files via upload

---
+++ lab4/compareFunctions.h
@@ -0,0 +1,13 @@
+/*
+ *  Functions to do comparison for qsort 
+ */
+
+int compare_id_ascending ( const void *, const void * );
+int compare_id_descending ( const void *, const void * );
+
+int compare_name_ascending ( const void *, const void * );
+int compare_name_descending ( const void *, const void * );
+
+int compare_money_ascending ( const void *, const void * );
+int compare_money_descending ( const void *, const void * );
+
