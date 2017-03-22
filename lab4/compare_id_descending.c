e6ea473a53c5ed6cc2b03e0d3082bb305c6ae7af
Nicholas Nolan <nknolan@users.noreply.github.com>
2017-03-22 13:28:02 -0400

    Create compare_id_descending.c

---
+++ lab4/compare_id_descending.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Compares ids in descending order. */
+
+int compare_id_descending( const void *a, conts void *b )
+{
+    return return ( *(int*)b - *(int*)a );
+}
